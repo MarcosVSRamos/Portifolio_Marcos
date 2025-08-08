5cd6c91fa66ee279b76412b322250dd44d523e96
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-08-07 21:59:08 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalCss from './styles'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <GlobalCss />
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/styles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const colors = {
+  white: '#dff9fb',
+  black: '#000',
+  grey: '#535c68',
+  blueBlack: '#130f40',
+  blue: '#30336b',
+  green: '#22a6b3'
+}
+
+export const breakpoints = {
+  desktop: '1024',
+  tablet: '768'
+}
+
+const GlobalCss = createGlobalStyle`
+  *{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Roboto, sans-serif;
+  list-style: none;
+}
+
+body {
+  background-color: ${colors.blueBlack};
+  color: ${colors.white};
+  }
+
+.container {
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    max-width: 80%;
+  }
+}
+`
+
+export default GlobalCss
